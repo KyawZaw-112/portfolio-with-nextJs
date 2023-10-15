feca3ad55ca69a5d11f344921adbf7e242df6c01
KyawZaw-112 <winkyawzaw660@gmail.com>
2023-10-15 15:27:44 +0630

    "feat: profile photo"

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,12 +1,14 @@
 "use client";
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
-import {Bona_Nova} from 'next/font/google';
+import { Bona_Nova } from "next/font/google";
 import BackGroundProfile from "./BackGroundProfile";
+import Image from "next/image";
+import profile from "./../app/public/profile-pic.jpg";
 
 const bonaNova = Bona_Nova({
-    subsets: ['greek'],
-    weight: "700"
+	subsets: ["greek"],
+	weight: "700",
 });
 
 type Props = {};
@@ -23,10 +25,18 @@ const Profile = ({}: Props) => {
 	});
 	return (
 		<div className=" h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
-            <BackGroundProfile />
+			<BackGroundProfile />
+			<Image
+				src={profile}
+				alt={"Profile-Picture"}
+				width={400}
+				height={400}
+				className=" relative rounded-full h-32 w-32 mx-auto object-cover"
+			/>
+			<h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
 			<h1>
-				<span className={bonaNova.className}>{text}</span>
-				<Cursor cursorColor="#fc5185" cursorStyle='_' />
+				<span className={`mr-4 ${bonaNova.className}`}>{text}</span>
+				<Cursor cursorColor="#fc5185" cursorStyle="_" />
 			</h1>
 		</div>
 	);
